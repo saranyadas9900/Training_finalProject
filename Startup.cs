3f203784fa76943d81e195a7bcbcb379f9717fe2
saranyadas9900 <163109842+saranyadas9900@users.noreply.github.com>
2024-08-20 11:37:19 +0530

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication4.Startup))]
+namespace WebApplication4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+
+        
+
+    }
+}
