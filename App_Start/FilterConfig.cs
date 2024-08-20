516f85379971e426aad62651dd1f19138b1ac33a
saranyadas9900 <163109842+saranyadas9900@users.noreply.github.com>
2024-08-20 11:30:16 +0530

    Add files via upload

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+
+namespace WebApplication4
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+            filters.Add(new NoCacheAttribute()); ;
+       
+
+        }
+    }
+}
