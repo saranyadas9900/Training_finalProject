a1a386f22b9c43cb3e308e682996ae56c73dc4e6
saranyadas9900 <163109842+saranyadas9900@users.noreply.github.com>
2024-08-20 11:32:25 +0530

    Add files via upload

---
+++ Filters/NoCacheAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+
+public class NoCacheAttribute : ActionFilterAttribute
+{
+    public override void OnResultExecuting(ResultExecutingContext filterContext)
+    {
+        filterContext.HttpContext.Response.Cache.SetExpires(DateTime.UtcNow.AddMinutes(-1));
+        filterContext.HttpContext.Response.Cache.SetCacheability(HttpCacheability.NoCache);
+        filterContext.HttpContext.Response.Cache.SetNoStore();
+        filterContext.HttpContext.Response.Cache.SetCacheability(HttpCacheability.NoCache);
+        filterContext.HttpContext.Response.Cache.SetNoStore();
+        base.OnResultExecuting(filterContext);
+    }
+}
